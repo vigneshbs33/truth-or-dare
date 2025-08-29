0994511a0f8c493e1b2794bfac555900c8e1e65c
vignesh <vignesh.bs06@gmail.com>
2025-08-30 04:09:04 +0530

    v25-hope its final

--- api/env.js
+++ api/env.js
@@ -1,6 +1,6 @@
-module.exports = (req, res) => {
+export default function handler(req, res) {
   res.status(200).json({
     SUPABASE_URL: process.env.SUPABASE_URL || null,
     SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY || null,
   });
-};
+}
